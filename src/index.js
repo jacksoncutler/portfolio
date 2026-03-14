684fc4bf93f5e1b6fc769e465fe31b561fcfd73c
Jackson <jackson.cutler@gmail.com>
2026-03-14 14:41:36 -0700

    Implement intersection observer for navbar highlighting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-const bgWave = document.getElementById('bg-wave');
+createObserver();
+createMediaQuery();
 
-window.matchMedia('(max-width: 800px)').addEventListener('change', (e) => {
-  console.log('hello');
-  if (e.matches) {
-    bgWave.setAttribute('viewBox', '0 0 350 150');
-  } else {
-    bgWave.setAttribute('viewBox', '0 0 600 150');
+function createObserver() {
+  const sectionList = document.querySelectorAll('.section');
+  const navList = document.querySelectorAll('.nav-item');
+
+  const io = new IntersectionObserver(observerCallback, {});
+  sectionList.forEach((sectionEl) => {
+    io.observe(sectionEl);
+  });
+
+  function observerCallback(entries) {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        const navEl = getNavEl(entry.target.id);
+        if (navEl) navEl.classList.add('intersecting');
+      } else {
+        const navEl = getNavEl(entry.target.id);
+        if (navEl) navEl.classList.remove('intersecting');
+      }
+      function getNavEl(sectionId) {
+        return Array.from(navList).find((navEl) => {
+          return sectionId === navEl.dataset.sectionId;
+        });
+      }
+    });
   }
-});
+}
+
+function createMediaQuery() {
+  const bgWave = document.getElementById('bg-wave');
+
+  window.matchMedia('(max-width: 800px)').addEventListener('change', (e) => {
+    if (e.matches) {
+      bgWave.setAttribute('viewBox', '0 0 350 150');
+    } else {
+      bgWave.setAttribute('viewBox', '0 0 600 150');
+    }
+  });
+}
