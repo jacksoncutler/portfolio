6ab25bcb1a781df4669214a40c0e5c6a75733fdd
Jackson <jackson.cutler@gmail.com>
2026-05-03 17:17:54 -0700

    Setup background wave pattern on page load

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-createObserver();
-createMediaQuery();
+createIO();
+setupWavePattern();
 
-function createObserver() {
-  const sectionList = document.querySelectorAll('.section');
+function createIO() {
+  const sectionList = document.querySelectorAll('.section.in-nav');
   const navList = document.querySelectorAll('.nav-item');
+  const options = { rootMargin: '-25% 0% -25% 0%' };
 
-  const io = new IntersectionObserver(observerCallback, {
-    rootMargin: '-25% 0% -25% 0%',
-  });
+  const io = new IntersectionObserver(observerCallback, options);
   sectionList.forEach((sectionEl) => {
     io.observe(sectionEl);
   });
 
   function observerCallback(entries) {
     entries.forEach((entry) => {
+      const navEl = getNavEl(entry.target.id);
       if (entry.intersectionRatio > 0) {
-        const navEl = getNavEl(entry.target.id);
         if (navEl) navEl.classList.add('intersecting');
       } else {
-        const navEl = getNavEl(entry.target.id);
         if (navEl) navEl.classList.remove('intersecting');
       }
     });
+
     function getNavEl(sectionId) {
       return Array.from(navList).find((navEl) => {
         return sectionId === navEl.dataset.sectionId;
@@ -30,14 +29,17 @@ function createObserver() {
   }
 }
 
-function createMediaQuery() {
+function setupWavePattern() {
   const bgWave = document.getElementById('bg-wave');
+  const mql = window.matchMedia('(max-width: 800px)');
+  mql.addEventListener('change', setViewBox);
+  setViewBox(mql);
 
-  window.matchMedia('(max-width: 800px)').addEventListener('change', (e) => {
-    if (e.matches) {
+  function setViewBox(mql) {
+    if (mql.matches) {
       bgWave.setAttribute('viewBox', '0 0 350 150');
     } else {
       bgWave.setAttribute('viewBox', '0 0 600 150');
     }
-  });
+  }
 }
